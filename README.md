2531e55f68edc66b87e8704be84452c407e83705
u7136563 <u7136563@anu.edu.au>
2020-04-05 00:13:25 +1100

    Updated main.py
    isvalid function introduced to check if number is correct

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ Steps - 1) Pick empty square
         2) Try all numbers and find the correct one that works
         3) Repeat for all squares
         4) If number is wrong, backtrack (There is no possible square for that value)
+
+Functions-
+
+isvalid => Check each column just like each element in row and is it equal to whatever the number is that we just added
+            in . When we are checking through board if it's position we just inserted we ignore that position but if it's
+            any other position that's gonna matter because that means there are two of same numbers in that row.
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 game = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0]
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
     [6, 0, 0, 1, 9, 5, 0, 0, 0],
     [0,9,8,0,0,0,0,6,0],
     [8,0,0,0,6,0,0,0,3],
@@ -10,3 +10,39 @@ game = [
     [0,0,0,0,8,0,0,7,9]
 ]
 
+def game_print(screen):
+    for row in range(len(screen)):
+        if row % 3 == 0 and row!=0:
+            print("- - - - - - - - - - - - -")
+
+        for column in range(len(screen[0])):
+            if column % 3 == 0 and column != 0:
+                print(" | ", end="")
+
+            if column == 8:
+                print(screen[row][column])
+            else:
+                print(str(screen[row][column])+" ", end="")
+
+
+def find_zero(screen):
+    for row in range(len(screen)):
+        for column in range(len(screen[0])):
+            if screen[row][column] == 0:
+                return row, column
+
+
+def isvalid(screen, number, position):
+
+    # Row
+    for column in range(len(screen[0])):
+        if screen[position[0]][column] == number and position[1] != column:
+            return False
+
+    # Column
+    for column in range(len(screen[0])):
+        if screen[column][position[1]] == number and position[0] != column:
+            return False
+
+    # Box
+    
