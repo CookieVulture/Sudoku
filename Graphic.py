1a658688b0b649666495dc33488bb2ca1c061947
u7136563 <u7136563@anu.edu.au>
2020-04-05 02:08:01 +1100

    Updated Graphical interface
    Basic grid working

--- Graphic.py
+++ Graphic.py
@@ -3,6 +3,7 @@ from main import backtracking, isvalid
 import time
 pygame.font.init()
 
+
 class Screen:
     game = [
     [5, 3, 0, 0, 7, 0, 0, 0, 0],
@@ -17,4 +18,51 @@ class Screen:
 ]
 
     def __init__(self, rows, cols, width, height):
-        None
+        self.rows = rows
+        self.cols = cols
+        self.width = width
+        self.height = height
+        self.model = None
+        self.cube = [[Cube(self.board[i][j], i, j, width, height) for j in range(cols)] for i in range(rows)]
+        self.selected = None
+
+    def update(self):
+        self.model = [[self.cube[i][j].value for j in range(self.cols)] for i in range(self.rows)]
+
+    def place(self, val):
+        row, col = self.selected
+        if self.cube[row][col].value == 0:
+            self.cube[row][col].set(val)
+            self.update()
+
+            if isvalid(self.model, val, (row, col)) and backtracking(self.model):
+                return True
+            else:
+                self.cube[row][col].set(0)
+                self.cube[row][col].set_temp(0)
+                self.update()
+                return False
+
+    def sketch(self, value):
+        row, col = self.selected
+        self.cube[row][col].set_temp(value)
+
+
+class Cube:
+    rows = 9
+    cols = 9
+
+    def __init__(self, value, row, col, width, height):
+        self.value = value
+        self.temp = 0
+        self.row = row
+        self.col = col
+        self.width = width
+        self.height = height
+        self.selected = False
+
+    def set(self, value):
+        self.value = value
+
+    def set_temp(self, value):
+        self.temp = value
